4cf633489af3a3a6de577f33132f85f8277347f1
demha1 <149089276+demha1@users.noreply.github.com>
2023-11-16 18:33:54 +0200

    Add files via upload

---
+++ main_window.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+window_main = QWidget()
+window_main.resize(600,300)
+window_main.setwindowTitle('Memori card')
+
+window_main.move(300,300)
+btn_main = QPushButton('menu')
